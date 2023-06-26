ee403bed22b95a43272a3f7f3767bcaab05a918c
cuevasnacho <102696702+cuevasnacho@users.noreply.github.com>
2023-06-26 00:01:55 -0300

    course exercises

--- famaf-comp/exercises/1581a.cpp
+++ famaf-comp/exercises/1581a.cpp
@@ -1,34 +1,40 @@
 #include <bits/stdc++.h>
+#ifdef DEMETRIO
+#define deb(...) fprintf(stderr,__VA_ARGS__)
+#define deb1(x) cerr << #x << " = " << x << endl
+#else
+#define deb(...) 0
+#define deb1(x) 0
+#endif
 #define pb push_back
+#define mp make_pair
 #define fst first
 #define snd second
-#define fore(i,a,b) for(int i=a,ggdem=b;i<ggdem;++i)
-#define fored(i,a,b) for(int i=a,ggdem=b;i>=ggdem;--i)
+#define fore(i,a,b) for(int i=a,ThxDem=b;i<ThxDem;++i)
 #define SZ(x) ((int)x.size())
-#define ALL(x) x.begin(),x.end()
-#define mset(a,v) memset((a),(v),sizeof(a))
-#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
 typedef long long ll;
-typedef pair<int,int> ii;
 
+const int MAXN = 2*1e5+7;
 const int MOD = 1e9+7;
-const int MAXN = 2*1e5+5;
 
 ll fact[MAXN];
 
 void f() {
-	fact[0]=1;
-	fore(i,1,MAXN) fact[i]=(fact[i-1]*i)%MOD;
+    fact[0]=1;
+    fore(i,1,MAXN)
+        fact[i]=(fact[i-1]*i)%MOD;
 }
 
-int main(){FIN;
-	int t; cin>>t;
-	f();
-	while(t--) {
-		int n; cin>>n;
-		cout<<fact[2*n]<<'\n';
-	}
+int main(){
+    int t; cin>>t;
+    f();
+    while(t--) {
+        ll n; cin>>n;
+        ll ans = (fact[2*n-1]*n)%MOD;
+
+        cout<<ans<<'\n';
+    }
 
 	return 0;
 }
---
+++ famaf-comp/exercises/cont1f.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define fst first
+#define snd second
+#define fore(i,a,b) for(int i=a,ggdem=b;i<ggdem;++i)
+#define fored(i,a,b) for(int i=a,ggdem=b;i>=ggdem;--i)
+#define SZ(x) ((int)x.size())
+#define ALL(x) x.begin(),x.end()
+#define mset(a,v) memset((a),(v),sizeof(a))
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> ii;
+
+#define INF 1e6
+#define oper min
+#define NEUT INF
+
+const int MAXN = 1e5+7;
+int a[MAXN];
+
+struct STree { // segment tree for min over integers
+	vector<int> st;int n;
+	STree(int n): st(4*n+5,NEUT), n(n) {}
+	void init(int k, int s, int e){
+		if(s+1==e){st[k]=0;return;}
+		int m=(s+e)/2;
+		init(2*k,s,m);init(2*k+1,m,e);
+		st[k]=oper(st[2*k],st[2*k+1]);
+	}
+	void upd(int k, int s, int e, int p, int v){
+		if(s+1==e){st[k]+=v;return;}
+		int m=(s+e)/2;
+		if(p<m)upd(2*k,s,m,p,v);
+		else upd(2*k+1,m,e,p,v);
+		st[k]=oper(st[2*k],st[2*k+1]);
+	}
+	int query(int k, int s, int e, int a, int b){
+		if(s>=b||e<=a)return NEUT;
+		if(s>=a&&e<=b)return st[k];
+		int m=(s+e)/2;
+		return oper(query(2*k,s,m,a,b),query(2*k+1,m,e,a,b));
+	}
+	void init(){init(1,0,n);}
+	void upd(int p, int v){upd(1,0,n,p,v);}
+	int query(int a, int b){return query(1,0,n,a,b);}
+}; // usage: STree rmq(n);rmq.init(x);rmq.upd(i,v);rmq.query(s,e);
+
+int main(){FIN;
+	int n,m; cin>>n>>m;
+    fore(i,0,m) cin>>a[i];
+
+    STree rmq(n); rmq.init();
+    int mini=0;
+    fore(i,0,m) {
+        rmq.upd(a[i]-1,1);
+        if(rmq.query(0,n)>mini) {mini++; cout<<1;}
+        else cout<<0;
+    }
+
+	return 0;
+}
